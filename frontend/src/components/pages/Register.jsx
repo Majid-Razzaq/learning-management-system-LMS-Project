3ba1bbf44bca929bc611a598dc04a7bd5cc42054
Majid-Razzaq <abdulmajid22770@gmail.com>
2025-07-25 20:51:53 +0500

    updated code

--- frontend/src/components/pages/Register.jsx
+++ frontend/src/components/pages/Register.jsx
@@ -1,40 +1,91 @@
 import React from 'react'
 import Layout from '../common/Layout'
 import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import { apiUrl } from '../common/Config'
 
 const Register = () => {
+
+    const{
+        handleSubmit, register, formState: {errors}, setError
+    } = useForm();
+
+    const onSubmit = async (data) => {
+        await fetch(`${apiUrl}/register`,{
+            method: 'POST',
+            headers:{
+                'Content-type' : 'application/json',
+                'Accept' : 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+        .then(res => res.json())
+        .then(result => {
+            console.log(result);
+            
+        })
+    };
+
   return (
     <Layout>
         <div className='container py-5 mt-5'>
             <div className='d-flex align-items-center justify-content-center'>
-                <form >
+                <form onSubmit={handleSubmit(onSubmit)}>
                     <div className='card border-0 shadow register'>
                         <div className='card-body p-4'>
                             <h3 className='border-bottom pb-3 mb-3'>Register</h3>
 
                             <div className='mb-3'>
                                 <label className='form-label' htmlFor="name">Name</label>
-                                <input                                     
+                                <input    
+                                {
+                                    ...register('name',{
+                                        required:"The name field is required",
+                                    })
+                                }                                 
                                 type="text" 
-                                className={`form-control`} 
-                                placeholder='Name' />                               
+                                className={`form-control ${errors.name && 'is-invalid'}`} 
+                                placeholder='Name'/>    
+                                {
+                                    errors.name && <p className='invalid-feedback'>{errors.name.message}</p>
+                                }                           
                             </div>
 
 
                             <div className='mb-3'>
                                 <label className='form-label' htmlFor="email">Email</label>
                                 <input 
-                                type="text" className={`form-control`} 
+                                {
+                                    ...register('email',{
+                                        required:"The email field is required",
+                                        pattern: {
+                                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                            message: "Invalid email address"
+                                        } 
+                                    })
+                                } 
+                                type="text" className={`form-control ${errors.email && 'is-invalid'}`} 
                                 placeholder='Email' />
+                                {
+                                    errors.email && <p className='invalid-feedback'>{errors.email.message}</p>
+                                } 
                                 
                             </div>
 
                             <div className='mb-3'>
                                 <label className='form-label' htmlFor="password">Password</label>
                                 <input 
+                                {
+                                    ...register('password',{
+                                        required:"The password field is required",
+                                    })
+                                } 
                                 type="password" 
-                                className={`form-control`} 
-                                placeholder='Password' />                                
+                                className={`form-control ${errors.password && 'is-invalid'}`} 
+                                placeholder='Password' />   
+                                {
+                                    errors.password && <p className='invalid-feedback'>{errors.password.message}</p>
+                                }                              
                             </div>
 
                             <div>
